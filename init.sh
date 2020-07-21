7a3ec59bf10543ed0bc95f3ee3847380582a248a
Tycho Alexander Docter <tadocter@gmail.com>
2020-07-21 23:56:01 +0200

    Update and rename disable.sh to init.sh

--- init.sh
+++ init.sh
@@ -7,3 +7,11 @@ defaults write com.apple.QuickTimePlayerX.LSSharedFileList RecentDocuments -dict
 defaults delete org.videolan.vlc.LSSharedFileList RecentDocuments
 defaults write org.videolan.vlc NSRecentDocumentsLimit 0
 defaults write org.videolan.vlc.LSSharedFileList RecentDocuments -dict-add MaxAmount 0
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+cd
+mkdir Documents/git
+ln -s Documents/git git
+cd ~/git
+git clone git@github.com:dd0c/homebrew-cask-upgrade.git
+/usr/local/bin/brew 
